6f3fc53cbd2d652c7f3dea200ea671423d4e871b
klibin <km.putnam2@gmail.com>
2022-10-28 13:57:56 -0600

    notes and added input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import { InputField } from './components/InputField';
+
 import './App.css';
 
-function App() {
+
+let name:string; 
+let age: number | string; //union type
+let hobbies: string[];
+
+let pesonName: unknown //recommended instead of any
+
+//function
+let printName: (name:string) => void //return type where void is...number, string, etc. 'never' returns NOTHING. void returns undefined.
+
+//tuple
+let role: [number, string];
+
+//object, use type or interface. it is a type of an alias. interface is the othe
+type Person = {
+  name: string;
+  age?: number;
+}
+
+interface Guy extends Person{
+  profession: string
+}
+
+//use types to combine 
+type X = {
+  a: string;
+  b: number;
+}
+
+//now contains all of 'y' PLUS props from type X. This also works between types and interfaces
+type Y = X & {
+  c: string;
+  d: number;
+}
+
+let y: Y = {
+  a: 'lls',
+  b: 4,
+  c: 'ddas',
+  d: 42
+}
+
+
+let person: Person = {
+  name: 'Kell',
+  age: 30
+}
+
+let lotsOfPeople: Person[];
+
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className= "heading">Taskify</span>
+      <InputField />
     </div>
   );
 }
