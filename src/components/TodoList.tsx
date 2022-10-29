3c50eaf534fe336af187f7106148fb6ed0e928ee
klibin <km.putnam2@gmail.com>
2022-10-29 10:40:21 -0600

    changed to have active and completed columns

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import { Todo } from '../model'
-import SingleTodo from './SingleTodo';
-import './styles.css'
-
+import React from "react";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+import "./styles.css";
 
 interface Props {
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
-export const TodoList = ({todos, setTodos}: Props) => {
+export const TodoList = ({ todos, setTodos }: Props) => {
   return (
-    <div className="todos">
+    <div className="container">
+      <div className="todos">
+        <span className="todosHeading">Active Tasks</span>
         {todos.map((todo) => (
-            <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
         ))}
+      </div>
+      <div className="todos remove">
+        <span className="todosHeading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
-
+    // <div className="todos">
+    //     {todos.map((todo) => (
+    //         <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
+    //     ))}
+    // </div>
+  );
+};
